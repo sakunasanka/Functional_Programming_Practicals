b8dacbc8c1d11898f1661f1c274573f3eddfa16b
Sakuna <140165351+sakunasanka@users.noreply.github.com>
2024-07-11 22:43:17 +0530

    q1.scala

--- labsheet4/q1.scala
+++ labsheet4/q1.scala
@@ -1,67 +1,40 @@
-object question_4_01{
-    var names: Array[String] = Array()
-    var quantities: Array[Int] = Array()
+object InventoryManagementSystem {
+  var itemNames: Array[String] = Array("Apple", "Banana", "Orange")
+  var itemQuantities: Array[Int] = Array(10, 5, 7)
 
-    def displayInventory():Unit={
-        var len = names.length
-        var i = 0
-        println("---------------Inventory---------------")
-        println("Name\t\tQuantity")
-        while(i<len){
-            println(s"${names(i)}\t\t${quantities(i)}")
-            i += 1
-        }
+  def displayInventory(): Unit = {
+    for (i <- itemNames.indices) {
+      println(s"${itemNames(i)}: ${itemQuantities(i)}")
     }
+    print("\n");
+  }
 
-    def restockItem(name: String , qty: Int):String={
-        for (nm <- names){
-            if(nm == name){
-              quantities(names.indexOf(nm)) += qty  
-              return s"Successfully added items to $name"
-            }
-        }
-        (s"$name not found in the existing inventories!!")
+  def restockItem(itemName: String, quantity: Int): Unit = {
+    val index = itemNames.indexOf(itemName)
+    if (index != -1) {
+      itemQuantities(index) += quantity
+    } else {
+      println("Item does not exist.")
     }
+  }
 
-    def sellItem(name: String , qty: Int):String={
-        for (nm <- names){
-            if(nm == name){
-                if(quantities(names.indexOf(nm)) < qty){
-                    return s"Not enough items: $name"
-                }else{
-                    quantities(names.indexOf(nm)) -= qty  
-                    return s"Successfully sell $qty $name items"
-                }
-            }
-        }
-        (s"$name not found in the existing inventories!!")
+  def sellItem(itemName: String, quantity: Int): Unit = {
+    val index = itemNames.indexOf(itemName)
+    if (index != -1) {
+      if (itemQuantities(index) >= quantity) {
+        itemQuantities(index) -= quantity
+      } else {
+        println("Not enough quantity to sell.")
+      }
+    } else {
+      println("Item does not exist.")
     }
+  }
 
-    def main(args: Array[String]):Unit={
-        names = Array("Book" , "Pen" , "Pencil")
-        quantities = Array(5 , 10 , 10)
-        
-        displayInventory()
-        println()
-
-        println(restockItem("Book" , 5)) //restock items when already existing product
-        displayInventory()
-        println()
-
-        println(restockItem("Ruler" , 2)) //restock items when not a already existing item
-        displayInventory()
-        println()
-
-        println(sellItem("Book" , 5)) // sell items when it is enough items
-        displayInventory()
-        println()
-
-        println(sellItem("Book" , 20)) // sell items when it is not enough items
-        displayInventory()
-        println()
-
-        println(sellItem("Ruler" , 5)) // sell items when not a already existing item
-        displayInventory()
-        println()
-    }
+  def main(args: Array[String]): Unit = {
+    displayInventory()
+    restockItem("Banana", 10)
+    sellItem("Apple", 3)
+    displayInventory()
+  }
 }
